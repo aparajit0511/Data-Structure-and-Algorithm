955854b7c9dc3d60fdd58591be6a24aa3847fc68
aparajit0511 <aparajit35@gmail.com>
2024-06-03 01:31:17 +0530

    bs prob

---
+++ Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,21 @@
+# Time Complexity - O(log N)
+# Space complexity - O(1)
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        start = 0
+        end = len(nums)-1
+
+        while start < end:
+            mid = (start + end) // 2
+
+            if nums[mid] > nums[end]:
+                start = mid + 1
+            else:
+                end = mid
+
+        return nums[start] 
+
+        
+                
