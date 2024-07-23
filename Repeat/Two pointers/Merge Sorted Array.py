fe409cf5a4b79ae4c00f295183b22f25859e185f
aparajit0511 <aparajit35@gmail.com>
2024-07-23 20:57:37 +0530

    revision

--- Repeat/Two pointers/Merge Sorted Array.py
+++ Repeat/Two pointers/Merge Sorted Array.py
@@ -1,41 +1,21 @@
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-
-        nums1 = nums1[:m]
-        if not m:
-            nums1.extend(nums2)
-
-        for num in nums2:
-            nums1.append(num)
-            print(nums1)
-
-        print("nums",nums1)
-
-        nums1.sort()
-
+# Time Complexity - O(M+N)
+# Space complexity - O(1)
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        
+        while m > 0 and n > 0:
 
-        if not m:
-            nums1 = []
-            nums1.extend(nums2)
-
-        nums1_count = 0
-        nums2_count = 0
-
-        while nums2_count < n-1:
-
-            if nums1[nums1_count] < nums2[nums2_count]:
-                nums1_count += 1
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
             else:
-                nums1.insert(nums1_count+1,nums2[nums2_count])
-                nums2_count+=1
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
 
-                
+        if n > 0:
+            nums1[:n] = nums2[:n]
+
