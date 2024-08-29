68ce888814fb0d126738ee3fdffdb453333df470
aparajit0511 <aparajit35@gmail.com>
2024-08-30 01:45:47 +0530

    heap

--- Repeat/Sliding window/Shortest Subarray with Sum at Least K.py
+++ Repeat/Sliding window/Shortest Subarray with Sum at Least K.py
@@ -30,3 +30,38 @@ class Solution:
 
 
         return min_ if min_ != float('inf') else -1
+
+
+
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        
+        from collections import deque
+
+        if len(nums) == 1:
+            return 1
+
+        queue = deque()
+        min_sub = float('inf')
+        sum = 0
+
+        prefix_sum = [0] * len(nums)
+        prefix_sum[0] = nums[0]
+
+        for i in range(1,len(nums)):
+            prefix_sum[i] = prefix_sum[i-1] + nums[i]
+
+        for i in range(len(nums)):
+
+            while queue and prefix_sum[i] >= k:
+                item = queue[0]
+                min_sub = min(i-item+1,min_sub)
+                # print(min_sub)
+                queue.popleft()
+
+            while queue and nums[i] < nums[queue[-1]]:
+                queue.pop()
+
+            queue.append(i)         
+
+        return -1 if min_sub == float('inf') else min_sub
