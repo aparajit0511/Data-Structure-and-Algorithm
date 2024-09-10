1fe58e90ae7c11bca2d66cf5cf9b6db293b10b1f
aparajit0511 <aparajit35@gmail.com>
2024-09-10 12:28:50 +0530

    LL

--- Repeat/Linked List/Merge k Sorted Lists.py
+++ Repeat/Linked List/Merge k Sorted Lists.py
@@ -1,39 +1,3 @@
-# Time Complexity - O(NlogN)
-# Space complexity - O(N)
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        result_array = []
-
-        for i in lists:
-            head = i
-            while head:
-                result_array.append(head.val)
-                head = head.next
-
-        result_array.sort()
-
-        ptr = head = None
-
-        for i in range(len(result_array)):
-
-            if head is None:
-                head = ListNode(result_array[i])
-                ptr = head
-            else:
-                ptr.next = ListNode(result_array[i])
-                ptr = ptr.next
-
-        return head
-
-
-
 # Time Complexity - O(Nlog K)
 # Space complexity - O(Nlog K)
 
