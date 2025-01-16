6b738330687627936d44e11dd4bf3603850ba51e
aparajit0511 <aparajit35@gmail.com>
2025-01-16 16:16:24 +0530

    contest

--- Contest/Maximum Multiplication Score.py
+++ Contest/Maximum Multiplication Score.py
@@ -1,8 +1,17 @@
 class Solution:
     def maxScore(self, a: List[int], b: List[int]) -> int:
-        
-        max_value = float("-inf")
-        
-        for i in range(len(a)):
+
+        return self.maxFind(a,b,float("-inf"),0,0)
+
+
+    def maxFind(self,a,b,maxValue,value,start):
+
+        if start == len(b)-1:
+            maxValue = max(value,maxValue)
+            return maxValue
+
+        i = 0
+        while i < len(a) and start < len(a):
+            value += self.maxFind()
+            i += 1
             
-        
